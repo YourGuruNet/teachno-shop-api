c8d61885174618b17516b59b5a1ff5e353481ef2
yourgurunet <info@yourgurunet.com>
2021-01-07 21:58:39 +0200

    instaled npm i --save stripe and conecting

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -1,8 +1,53 @@
-'use strict';
+const API_KEY = process.env.REACT_APP_API_KEY;
+("use strict");
 
+const { default: createStrapi } = require("strapi");
+
+const stripe = require("stripe")(
+  "sk_test_51I5YdXFhnjRgMEaiVZfeJMY2b8K5a0Qq71Qu7xZE1xXWE6UGwTLd1l37tZ5PRBLD4bXRKw8vywLPkqCQCxPOlc6q00rFDbbMKz"
+);
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  create: async (ctx) => {
+    const {
+      name,
+      phone,
+      country,
+      city,
+      address,
+      postcode,
+      cartName,
+      total,
+      items,
+      info,
+      stripeTokenId,
+    } = ctx.request.body;
+
+    const { id } = ctx.state.user;
+    //console.log(ctx.state);
+    const charge = await stripe.charges.create({
+      amount: Math.round(total * 100),
+      currency: "eur",
+      source: stripeTokenId,
+      description: `Order ${new Date()} made by ${ctx.state.user.username}`,
+    });
+    const order = await strapi.services.order.create({
+      name,
+      phone,
+      country,
+      city,
+      address,
+      postcode,
+      cartName,
+      total,
+      items,
+      info,
+      user: id,
+    });
+    return order;
+  },
+};
